de3407fc278cb767a6e53d8352f0339e25ca6cdd
Acromion07 <yasinokunakol@gmail.com>
2021-08-21 00:02:36 +0300

    first commit navbar started

--- tour-app/src/components/navbar/Navbar.js
+++ tour-app/src/components/navbar/Navbar.js
@@ -1,3 +1,14 @@
 import React from "react";
 
-const 
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <a href="aboutus">ABOUT US</a>
+            <a href="foryou">FOR YOU</a>
+            <a href="services">SERVİCES</a>
+            <a href="blog">BLOG</a>
+            <a href="vlog">VLOG</a>
+            <a href="contact">CONTACT</a>
+        </div>
+    )
+}
