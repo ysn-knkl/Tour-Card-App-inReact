d7685c380b662bdcc1ef6c31bd579db0c46ced38
Acromion07 <yasinokunakol@gmail.com>
2021-08-21 02:13:27 +0300

    cards -header- data-favicon components added

---
+++ tour-app/src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="big-title">
+            <h3>
+                POPULER TOUR PLACES
+            </h3>
+        
+        </div>
+    );
+};
+
+export default Header;
